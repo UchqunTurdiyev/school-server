0ca308cab5666e040ca5731e7ba64f8d279c8cea
uchqun007 <uchqunjonturdiev@gmail.com>
2023-08-13 19:14:49 +0500

    controller function

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  blogs: BlogDto[];
+
+  constructor() {
+    this.blogs = [
+      {
+        id: 1,
+        title: 'NestJS',
+        excerpt: 'NestJS Full Courses',
+        description: 'NestJS Full course from 0 to hero',
+      },
+      {
+        id: 2,
+        title: 'NextJS',
+        excerpt: 'NextJS Full Courses',
+        description: 'NextJS Full course from 0 to hero',
+      },
+      {
+        id: 3,
+        title: 'NuxtJS',
+        excerpt: 'NuxtJS Full Courses',
+        description: 'NuxtJS Full course from 0 to hero',
+      },
+    ];
+  }
+
+  @HttpCode(200)
+  @Get()
+  async getAll() {
+    return this.blogs;
+  }
+
+  @HttpCode(201)
+  @Post()
+  async create(@Body() dto: BlogDto) {
+    const data: BlogDto = {
+      id: new Date().getTime(),
+      ...dto,
+    };
+
+    return [...this.blogs, data];
+  }
+}
